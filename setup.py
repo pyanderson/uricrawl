e45eff0edbe2c2e1f60713c34a946bb48c3d5c5b
Anderson <anderson.sl93@hotmail.com>
2016-09-28 23:27:28 -0300

    fix setup

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='uritool',
       license='MIT',
       install_requires=['scrapy', 'python-slugify', 'jinja2'],
       packages=['uritool', ],
+      include_package_data=True,
       zip_safe=False,
       entry_points={
           'console_scripts': ['uritool=uritool.cmdline:main']
